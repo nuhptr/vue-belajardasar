71cef918884077f37451e6c38964b68d336ee9a2
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2022-11-10 10:40:02 +0700

    pinia learn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+// Compositon API
+import { useProductStore } from "@/stores/product";
+import { useCounterStore } from "@/stores/counter";
+
+const counter = useCounterStore();
+const product = useProductStore();
+
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="about"></div>
   </main>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
